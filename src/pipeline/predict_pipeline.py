d89a74b77843edc01e121ae7619a81819614690a
sohal-sandy <sandeepsohal.k@gmail.com>
2025-03-29 23:52:33 -0400

    flask app

---
+++ src/pipeline/predict_pipeline.py
@@ -0,0 +1,11 @@
+import sys
+import pandas as pd
+from src.exception import CustomException
+from src.uti.s import load_object
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+
+class CustomData:
+    def __init__( self, )
